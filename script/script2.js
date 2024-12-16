6de9a3d1fb0a295b8a3bbc9d11646de05291aac1
Darlison1398 <drsilva123jd@gmail.com>
2024-12-16 15:34:58 -0300

    Ultima atualização do dell

--- script/script2.js
+++ script/script2.js
@@ -2,4 +2,5 @@ const btn  = window.document.getElementById("btn");
 
 btn.addEventListener('click', () =>{
     alert('Esse botãod eve aparecer no projeto do notebook dell também.')
+    btn.style.color = 'red';
 })
